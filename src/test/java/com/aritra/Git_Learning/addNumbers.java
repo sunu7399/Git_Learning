e13591008b6484fb76f8d0c78cea140227259251
Aritra Pal <aritrathecosmo@gmail.com>
2021-12-19 16:21:04 +0530

    1st time commit

---
+++ src/test/java/com/aritra/Git_Learning/addNumbers.java
@@ -0,0 +1,13 @@
+package com.aritra.Git_Learning;
+
+public class addNumbers {
+	
+
+	
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println(c);
+	}
+}
