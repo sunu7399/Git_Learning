2fcabc3c663749a5b1f9bff88ab41aa21d5ebfee
Aritra Pal <aritrathecosmo@gmail.com>
2021-12-19 17:10:22 +0530

    add nub modi& multiply added

--- src/test/java/com/aritra/Git_Learning/multiplyNumbers.java
+++ src/test/java/com/aritra/Git_Learning/multiplyNumbers.java
@@ -1,13 +1,14 @@
 package com.aritra.Git_Learning;
 
-public class addNumbers {
+public class multiplyNumbers {
 	
 
 	
 	public static void main(String[] args) {
 		int a=10;
 		int b=20;
-		int c=a+b;
+		int c=a*b;
+		
 		System.out.println(c);
 	}
 }
