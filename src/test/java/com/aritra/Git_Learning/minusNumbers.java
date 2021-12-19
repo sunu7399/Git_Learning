0ad75a547d24389bba0f771d80719231dcac4028
Aritra Pal <aritrathecosmo@gmail.com>
2021-12-19 16:38:34 +0530

    minus commit

---
+++ src/test/java/com/aritra/Git_Learning/minusNumbers.java
@@ -0,0 +1,13 @@
+package com.aritra.Git_Learning;
+
+public class minusNumbers {
+	
+
+	
+	public static void main(String[] args) {
+		int a=30;
+		int b=20;
+		int c=a-b;
+		System.out.println(c);
+	}
+}
